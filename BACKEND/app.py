7d9ce3be9ab78b2d52ab3cf07a70308dce869076
Anamika Yadav <anamikaapinny12@gmail.com>
2026-08-09 18:56:34 +0530

    Fix production CORS

--- BACKEND/app.py
+++ BACKEND/app.py
@@ -5,12 +5,8 @@ app =FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "https://myproject-1-881m.onrender.com",
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
